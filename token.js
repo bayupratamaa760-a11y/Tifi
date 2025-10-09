2ae163838163fda23438a7e5daf6389fcd1c35cc
bayupratamaa760-a11y <bayupratamaa760@gmail.com>
2025-10-09 16:05:23 +0700

    Update token list

--- token.js
+++ token.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8091926248:AAG_-bmvD_7STAtlgni-bZWM_72l22qUypA"
+    "8091926248:AAG_-bmvD_7STAtlgni-bZWM_72l22qUypA",
+    "8387625298:AAHuT5LSIj3hW-CTqX49lyrkUfSBlWyeCy4"
   ]
 }
