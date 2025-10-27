bf3d27be235f34e81ecdababc3d43bcd023dfce3
bayupratamaa760-a11y <bayupratamaa760@gmail.com>
2025-10-27 13:42:20 +0700

    Update token.js

--- token.js
+++ token.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8398373045:AAG8BllyYNejky6_8YI2aIlm-6qfllzhMpw",
-    "8013114098:AAGsikKCz4jgv5r1byPeQE4mu8F0yCuai-E"
+    "8013114098:AAGsikKCz4jgv5r1byPeQE4mu8F0yCuai-E",
+    "8221818396:AAECcg8DR4_TJSL4Dj23PkHa9EJBUEfZ0Dg"
   ]
 }
